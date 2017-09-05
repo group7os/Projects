f0691ef5d84c168477c576cee113f69336517add
root <root@localhost.localdomain>
2017-09-05 07:45:11 -0700

    up to step 5 in project 2: Code reading

---
+++ project2/cs161/cs161-gcc/gcc/.gdbinit
@@ -0,0 +1,3 @@
+dir .
+dir /root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc
+source /root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/gdbinit.in
---
+++ project2/cs161/cs161-gcc/gcc/mddeps.mk
@@ -0,0 +1,64 @@
+MD_INCLUDES = \
+	/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/predicates.md \
+	/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4k.md \
+	/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/5k.md \
+	/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/24k.md \
+	/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/3000.md \
+	/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4000.md \
+	/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4100.md \
+	/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4130.md \
+	/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4300.md \
+	/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4600.md \
+	/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/5000.md \
+	/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/5400.md \
+	/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/5500.md \
+	/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/6000.md \
+	/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/7000.md \
+	/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/9000.md \
+	/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/sb1.md \
+	/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/sr71k.md \
+	/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/generic.md \
+	/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/mips-ps-3d.md \
+	/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/mips-dsp.md
+
+/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/predicates.md:
+
+/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4k.md:
+
+/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/5k.md:
+
+/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/24k.md:
+
+/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/3000.md:
+
+/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4000.md:
+
+/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4100.md:
+
+/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4130.md:
+
+/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4300.md:
+
+/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4600.md:
+
+/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/5000.md:
+
+/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/5400.md:
+
+/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/5500.md:
+
+/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/6000.md:
+
+/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/7000.md:
+
+/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/9000.md:
+
+/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/sb1.md:
+
+/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/sr71k.md:
+
+/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/generic.md:
+
+/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/mips-ps-3d.md:
+
+/root/Documents/Projects/project2/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/mips-dsp.md:
